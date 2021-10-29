299991af5d925748183c6cdcc67a5f5e32aa38bd
DCrawford <daniel.crawford@microsoft.com>
2021-10-29 17:24:36 -0400

    Update FindOverPartitionedCCITables.sql
    Changed logic to look at total count of partitions instead of Max(Partition_Number).  Also corrected join condition for sys.indexes to include index_id on [sys].[dw_pdw_nodes_db_partition_stats] to prevent scenarios for multiple indexes on a table presenting as extra partitions since changing to use COUNT() instead of MAX().

--- Synapse/SynapseOverPartitionedTables/FindOverPartitionedCCITables.sql
+++ Synapse/SynapseOverPartitionedTables/FindOverPartitionedCCITables.sql
@@ -1,15 +1,16 @@
 SELECT 
        SCHEMA_NAME(t.schema_id) as SchemaName
        ,t.name as TableName
-		,MAX(partition_number) as PartitionCount
+		,Count(partition_number)/60 as PartitionCount
+		,MAX(partition_number) as PartitionCount_Original
        ,SUM(row_count) as [Row_Count]
 FROM sys.[dm_pdw_nodes_db_partition_stats] nps
 INNER JOIN sys.[pdw_nodes_tables] nt on nt.object_id = nps.object_Id and nt.distribution_id = nps.distribution_id
 INNER JOIN sys.pdw_table_mappings tm on tm.physical_name = nt.name
 INNER JOIN sys.tables t on tm.object_id = t.object_id
-INNER JOIN sys.indexes i on i.object_id = t.object_id
+INNER JOIN sys.indexes i on i.object_id = t.object_id and nps.index_id = i.index_id
 WHERE i.index_id = 1 and i.type_desc = 'CLUSTERED COLUMNSTORE'
-GROUP BY schema_name(t.schema_id), t.name--,nps.distribution_id, nps.partition_number
-HAVING SUM(row_count)/MAX(partition_number) <60000000 -- include tables where there are at least 60m records per partition 
-	and MAX(partition_number) > 1 -- filter out non-partitioned tables
+GROUP BY schema_name(t.schema_id), t.name --,nps.distribution_id, nps.partition_number
+HAVING SUM(row_count)/(count(partition_number)/60) <60000000 -- include tables where there are at least 60m records per partition 
+	and COUNT(partition_number)/60 > 1 -- filter out non-partitioned tables
 	and SUM(row_count) > 0 -- filter out tables with 0 records
